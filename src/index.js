8775d493ca3865359fb469f6d782aeec8b54d719
markoter <markoterdev@outlook.com>
2022-08-08 17:37:25 +0200

    updt to react18 (createRoot instead of DOM.render)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
 
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+// this is not supported anymore:
+// ReactDOM.render(<App/>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
